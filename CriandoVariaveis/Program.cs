1583021955d72f5a4a4908e9b7d5ded102d57b87
eliveltonoliveira <eliveltonfalc94@gmail.com>
2019-11-28 17:57:02 -0300

    Add files via upload

---
+++ CriandoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola novo teste");
+
+            int idade = 25;
+
+            // Console.WriteLine(idade);
+
+            Console.WriteLine("A idade é" + idade + ",parabens!");
+
+            Console.WriteLine("Ao mostrar idade. Tecle enter para Sair!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestaPontoFlutuante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            double salario = 1250.70;
+            // Console.WriteLine(salario);
+
+            Console.WriteLine("Meu salario é" + salario);
+
+            //int divisao = 5 / 2;
+            // Console.WriteLine(divisao);
+
+            double divisao = 5.0 / 2;
+            Console.WriteLine(divisao);
+        }
+    }
+}
